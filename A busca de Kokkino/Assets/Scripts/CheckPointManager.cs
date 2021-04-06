9579ca58263cce1a5d66f1bce8bb8b621b488c3c
Raphael Marchetti Calciolari <79259612+raphazik4@users.noreply.github.com>
2021-04-06 20:14:34 -0300

    Ajustes para implementacão de checkpoint, e fader
    - BUG no colisor, Checkpoint não detecta quando a kokkino colide com ele

---
+++ A busca de Kokkino/Assets/Scripts/CheckPointManager.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointManager : MonoBehaviour
+{
+    //Scrip associado a um objeto dentro da cena
+    private static CheckPointManager instance;
+    public Vector2 lastCheckpointPos;               //Armazena a posicão do ultimo checkpoint atingido
+    public List<Vector2> checkPointactivated;       //Armazena uma lista com a posicão de todos os checkpoints salvos (para previnir o ressalvamento)
+
+    void Awake() //Garante a existência de apenas um checkpointmanager (que mantém suas propriedades ao recarregar cenas)
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        //Seria possível somente verificar os checkpoints uma vez a cada recarregamento de cena, precisa melhorar...
+        correctCheckpoints(checkPointactivated);
+    }
+
+    private void correctCheckpoints(List<Vector2> checks)
+    {
+        //Compara a posicão de cada checkpoint (recriados a cada recarregamento da cena) com os salvos anteriormente e desativa os já salvos
+        GameObject[] checkpoints = GameObject.FindGameObjectsWithTag("Checkpoint");
+        foreach (Vector2 check in checks)
+        {
+            for (int i = 0; i < checkpoints.Length; i++)
+            {
+                Checkpoint esteCheckpoint = checkpoints[i].GetComponent<Checkpoint>();
+                if (check.Equals(esteCheckpoint.transform.position))
+                {
+                    esteCheckpoint.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ A busca de Kokkino/Assets/Scripts/FaderController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class FaderController : MonoBehaviour
+{
+    private GameObject Player;
+    void Start()
+    {
+        //Cria a instância do protagonista
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Verifica uma vez por frame se o jgoador morreu e inicia a animacão (da transicão de tela na morte)
+        if (Player.GetComponent<PlayerController>().Poison())
+        {
+            //Fader controler está associado a uma imagem (filha do canvas), esta imagem escurece e esclarece
+            this.gameObject.GetComponent<Animator>().SetTrigger("Morte");
+        }
+    }
+}
