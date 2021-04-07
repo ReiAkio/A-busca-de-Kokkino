deed92f40958580a12759c9bc80cf162799a93f9
Comunalti <frps2003@gmail.com>
2021-04-06 22:24:16 -0300

    galho novo
    galho novo

---
+++ A busca de Kokkino/Assets/Scripts/CustomPlayer/PlayerAnimationController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private Animator _animator;
+    private PlayerController2 _playerController;
+    public string currentAnimation;
+
+    public float AnimationSpeed = 1;
+    public float speedMultiplier = 1;
+    private void Start()
+    {
+        _playerController = gameObject.GetComponent<PlayerController2>();
+        _animator = gameObject.GetComponent<Animator>();
+        _animator.Play("Walk_Left");
+    }
+
+    private void Update()
+    {
+        string nextAnimation = "";
+        if (_playerController.IsWalking)
+        {
+            if (_playerController.HorizontalMovement > 0)
+            {
+                nextAnimation = "Walk_Right";
+            }
+            else
+            {
+                nextAnimation = "Walk_Left";
+            }
+        }else if (_playerController.IsIdle)
+        {
+            if (currentAnimation == "Walk_Right")
+            {
+                nextAnimation = "Idle_Right";
+            }
+            
+            if (currentAnimation == "Walk_Left")
+            {
+                nextAnimation = "Idle_Left";
+            }
+        }
+
+        AnimationSpeed = _playerController.CurrentSpeed * speedMultiplier;
+        print(nextAnimation);
+        if (currentAnimation != nextAnimation)
+        {
+            _animator.Play(nextAnimation);
+            _animator.speed = AnimationSpeed;
+        }
+
+        currentAnimation = nextAnimation;
+    }
+}
---
+++ A busca de Kokkino/Assets/Scripts/CustomPlayer/PlayerController2.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class PlayerController2 : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    //private AnimationType currentAnimation;
+    [Header("Custom Properites for PlayerController")]
+    public float JumpForce = 50;
+    public float Speed = 20;
+    public float RunningSpeed = 40;
+    public int RemainJump = 0;
+    public int MaxJump = 2;
+    public float JumpingGratityScale = 8;
+    public float DefaultGratityScale = 12;
+    public float HorizontalMovement;
+    public float CurrentSpeed;
+
+    [Header("Player movement status")]
+    public bool IsJumping;
+    public bool IsFalling;
+    public bool IsWalking;
+    public bool IsRunning;
+    public bool IsGrounded
+    {
+        get { return GroudUnderPlayer.Count > 0; }
+    }
+    public bool IsIdle
+    {
+        get { return IsGrounded && !IsJumping && !IsFalling && !IsWalking && !IsRunning; }
+    }
+    
+    public List<GameObject> GroudUnderPlayer = new List<GameObject>(); 
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        List<RaycastHit2D> results = new List<RaycastHit2D>();
+        Physics2D.Raycast(transform.position, -Vector2.up,new ContactFilter2D(),results);
+        foreach (RaycastHit2D hit2D in results)
+        {
+            if (hit2D.collider ==  other.collider)
+            {
+                CollidableObject collidableObject = other.gameObject.GetComponent<CollidableObject>();
+                if (collidableObject)
+                {
+                    IsFalling = false;
+                    RemainJump = MaxJump;
+                    GroudUnderPlayer.Add(other.gameObject);
+                }   
+            }
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        GroudUnderPlayer.Remove(other.gameObject);
+    }
+    void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.S))
+        {
+           
+        }
+
+        if (IsJumping && _rigidbody.velocity.y <= 0)
+        {
+            IsJumping = false;
+            IsFalling = true;
+        }
+
+
+        bool JumpCommand = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W);
+        if ( JumpCommand && RemainJump > 0)
+        {
+            RemainJump -= 1;
+            IsJumping = true;
+            _rigidbody.velocity += Vector2.up * JumpForce;
+        }
+
+        if (IsJumping && Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody.gravityScale = JumpingGratityScale;
+        }
+        else
+        {
+            _rigidbody.gravityScale = DefaultGratityScale;
+        }
+
+        HorizontalMovement = Input.GetAxis("Horizontal");
+        
+        bool RunningButton = Input.GetKey(KeyCode.LeftControl);
+
+
+        Vector2 offset;
+        if (RunningButton)
+        {
+            CurrentSpeed = RunningSpeed;
+            offset = new Vector2(HorizontalMovement * RunningSpeed * Time.deltaTime,0);
+        }
+        else
+        {
+            CurrentSpeed = Speed;
+            offset = new Vector2(HorizontalMovement * Speed * Time.deltaTime,0);
+        }
+
+        if (offset.x == 0)
+        {
+            IsWalking = false;
+            IsRunning = false;
+        }else if (RunningButton)
+        {
+            IsRunning = true;
+            IsWalking = false;
+        }
+        else
+        {
+            IsRunning = false;
+            IsWalking = true;
+        }
+        _rigidbody.position += offset;
+        _rigidbody.velocity = new Vector2(
+            Mathf.Clamp(_rigidbody.velocity.x, -60, 60),
+            Mathf.Clamp(_rigidbody.velocity.y, -60, 50));
+        
+        // AnimationType thisFrameAnimation = GetThisFrameAnimation();
+        // if (thisFrameAnimation != currentAnimation)
+        // {
+        //     currentAnimation = thisFrameAnimation;
+        //     ChangingAnimationEvent?.Invoke(thisFrameAnimation,MovingDirection);
+        // }
+    }
+
+    // private AnimationType GetThisFrameAnimation()
+    // {
+    //     
+    // }
+      
+    // public enum AnimationType
+    // {
+    //     walk,
+    //     idle,
+    //     jump,
+    //     falling,
+    // }
+    // public delegate void SampleEventHandler(AnimationType animationType,Vector2 direction);
+    // public event SampleEventHandler ChangingAnimationEvent;
+}
