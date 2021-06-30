af1b327cd3e4e71ffd063f2747f2cad20c22cf52
CindyYoshita <cindynat11@gmail.com>
2021-06-30 00:15:25 -0300

    PortaFim

---
+++ A busca de Kokkino/Assets/Scripts/Objects/Door/Door.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Application.Quit();
+            Debug.Log("Fim");
+        }
+    }
+}
