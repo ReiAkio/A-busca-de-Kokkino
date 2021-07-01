418c0f7ef59efeb0973a4fa9f69e00615b5dcf6d
Raphael Marchetti Calciolari <79259612+raphazik4@users.noreply.github.com>
2021-06-30 22:34:30 -0300

    Corrigido Porcamente o erro de load

--- A busca de Kokkino/Assets/Scripts/Menu/MenuStart.cs
+++ A busca de Kokkino/Assets/Scripts/Menu/MenuStart.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 public class MenuStart : MonoBehaviour
 {
     public string LoadScene;
+    public KeyObject key_1;
+    public KeyObject key_2;
+    public KeyObject key_3;
+    public InventoryObject invetory;
     public void HandlePlayButtonOnClickEvent()
     {
         SceneManager.LoadScene(LoadScene);
@@ -12,6 +16,10 @@ public class MenuStart : MonoBehaviour
 
     public void HandleNewGameButtonOnClickEvent()
     {
+        key_1.wasCollected = false;
+        key_2.wasCollected = false;
+        key_3.wasCollected = false;
+        invetory.itemCollectedQuantity = 0;
         Vector3 initialPosition = new Vector3(-20.37655f, 2f, 0f);
         PlayerData defaultSave = new PlayerData(initialPosition);
         SaveSystem.saveData(defaultSave);
