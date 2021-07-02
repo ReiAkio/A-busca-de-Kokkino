1921c1ed43abd1047339c1d2b7f949e3ae736e6c
ReiAkio <tanaka1557@gmail.com>
2021-07-01 22:24:01 -0300

    fader adicionado

--- A busca de Kokkino/Assets/Scripts/Fader/FaderController.cs
+++ A busca de Kokkino/Assets/Scripts/Fader/FaderController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class FaderController : MonoBehaviour
 {
     private GameObject Player;
+
     void Start()
     {
         //Cria a instância do protagonista
@@ -10,13 +11,11 @@ public class FaderController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+
+
+    public void fader()
     {
-        //Verifica uma vez por frame se o jgoador morreu e inicia a animacão (da transicão de tela na morte)
-        if (Player.GetComponent<PlayerController2>().IsDead)
-        {
-            //Fader controler está associado a uma imagem (filha do canvas), esta imagem escurece e esclarece
-            this.gameObject.GetComponent<Animator>().SetTrigger("HasDied");
-        }
+        //Fader controler está associado a uma imagem (filha do canvas), esta imagem escurece e esclarece
+        this.gameObject.GetComponent<Animator>().SetTrigger("HasDied");
     }
 }
